f2d7246859d89f1c70cf911eeb75daa7575886d8
jeong eun <lje7370@gamil.com>
2025-04-18 15:32:24 +0900

    초기 세팅

---
+++ src/main/java/org/project/gowhere/GoWhereApplication.java
@@ -0,0 +1,13 @@
+package org.project.gowhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoWhereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoWhereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/gowhere/GoWhereApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.gowhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoWhereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
